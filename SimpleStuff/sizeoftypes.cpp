29d120df82fb589640ddead160567d5609e731d2
Kamal Sharma <kamaldgrt@gmail.com>
2020-11-30 23:15:12 +0530

    Create sizeoftypes.cpp

---
+++ SimpleStuff/sizeoftypes.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Size of char : " << sizeof(char) << " \n";
+    cout << "Size of int : " << sizeof(int) << " \n";
+    cout << "Size of float : " << sizeof(float) << " \n";
+    cout << "Size of long : " << sizeof(long) << " \n";
+    cout << "Size of double : " << sizeof(double) << " \n";
+    cout << "Size of long double : " << sizeof(long double) << " \n";
+    cout << "Size of long long int : " << sizeof(long long int) << " \n";
+  
+    cout << "Size of unsigned char : " << sizeof(unsigned char) << " \n";
+    cout << "Size of unsigned int : " << sizeof(unsigned int) << " \n";
+
+    return 0;
+}

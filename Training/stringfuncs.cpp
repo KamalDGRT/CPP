ea8215b31cb6fb051c191fd05b400510af4278c3
Kamal Sharma <kamaldgrt@gmail.com>
2020-12-05 09:39:20 +0530

    create stringfus.cpp

---
+++ Training/stringfuncs.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    string s1, s2;
+    getline(cin,s1);
+    getline(cin,s2);
+    swap(s1,s2);                                                               
+    cout << s1 << endl << s2 << endl;
+    cout << s1.length() << endl << s2.length() << endl;
+    cout << s1.find("a") << endl;
+    cout << s2.find("a") << endl;
+    s1.erase();
+    s2.erase(1,4);
+    cout << s1 << s2 <<endl;
+    return 0;
+}
